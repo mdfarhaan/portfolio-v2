008ada29ecc1c6a5533ef5c0396baacc3b1144ca
mdfarhaan <farhaanm110@gmail.com>
2023-06-13 03:12:02 +0530

    :bug: Fix Marquee Component

--- components/Landing/Skills.tsx
+++ components/Landing/Skills.tsx
@@ -1,40 +1,39 @@
 import React from "react";
-import Marquee from "react-easy-marquee";
+import Marquee from "react-fast-marquee";
+import { useMediaQuery } from "react-responsive";
 import { Heading, Section } from "../Shared";
 import { skills } from "../helper/data";
 
 const Skills = () => {
+  const isMobile = useMediaQuery({ query: "(max-width: 600px)" });
+
   return (
     <Section id="skills-section">
       <Heading title="Skills" />
-      <div className="w-[90%]">
-        {skills.map((item, idx) => (
-          <div key={item.section} className="mt-20 text-white">
-            <h3 className="self-start text-3xl mb-10 tracking-widest font-normal">
-              {item.section}
-            </h3>
-            <Marquee
-              height="200px"
-              reverse={idx % 2 == 0 ? true : false}
-              duration={18000}
-              align="center"
-            >
-              {item.skills.map((skill) => (
-                <div className="grid place-items-center" key={skill}>
-                  <img
-                    src={`/skills/${skill}.svg`}
-                    alt={skill}
-                    className="w-20 mx-20 h-20"
-                  />
-                  <span className="text-lg font-light text-center mt-2">
-                    {skill}
-                  </span>
-                </div>
-              ))}
-            </Marquee>
-          </div>
-        ))}
-      </div>
+      {skills.map((item, idx) => (
+        <div key={item.section} className="mt-20 text-white">
+          <h3 className="self-start text-3xl mb-10 tracking-widest font-normal">
+            {item.section}
+          </h3>
+          <Marquee
+            style={{ width: isMobile ? "150vw" : "80vw" }}
+            direction={idx % 2 == 0 ? "left" : "right"}
+          >
+            {item.skills.map((skill) => (
+              <div className="grid place-items-center" key={skill}>
+                <img
+                  src={`/skills/${skill}.svg`}
+                  alt={skill}
+                  className="w-20 md:mx-20 mx-10 h-20"
+                />
+                <span className="text-lg font-light text-center mt-2">
+                  {skill}
+                </span>
+              </div>
+            ))}
+          </Marquee>
+        </div>
+      ))}
     </Section>
   );
 };
